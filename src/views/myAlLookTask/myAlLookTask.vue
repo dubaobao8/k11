2905caaf1bfa737ad5cddf4791aedb9206230e60
DuXin <610642087@qq.com>
2020-04-06 02:15:08 +0800

    我的审阅

--- src/views/myAlLookTask/myAlLookTask.vue
+++ src/views/myAlLookTask/myAlLookTask.vue
@@ -3,77 +3,74 @@
     <main class="bg-white">
       <!-- 搜索框 -->
       <section class="search-wrp">
-        <v-search-new @select="onSelect" class="search"/>
+        <v-search-new @select="onSelect" class="search" />
       </section>
 
       <!-- 列表 -->
       <section class="scroll-wrp">
-        <v-list-reports :extra-params="extraParams" :url="url"/>
+        <v-list-reports-new :extra-params="extraParams" :url="url"/>
       </section>
-
     </main>
-    <tab-bar class="tab-bar" curTab="myAlLookTask"/>
+    <tab-bar class="tab-bar" curTab="myAlLookTask" />
   </div>
 </template>
 
 <script>
-  import VSearchNew from "@/components/search/v-search-new";
-  import TabBar from "@/components/tab-bar";
-  import VListReports from "@/components/srcollList/v-list-reports";
+import VSearchNew from "@/components/search/v-search-new";
+import TabBar from "@/components/tab-bar";
+import VListReportsNew from "@/components/srcollList/v-list-reports-new";
 
-  export default {
-    name: "mine",
-    components: {VListReports, VSearchNew, TabBar},
-    data() {
-      return {
-        url: "/api/Statistics/AlCheckList",
-        extraParams: {},
+export default {
+  name: "mine",
+  components: { VListReportsNew, VSearchNew, TabBar },
+  data() {
+    return {
+      url: "/api/Statistics/AlCheckList",
+      extraParams: {}
+    };
+  },
+  methods: {
+    // 搜索框选择完毕
+    onSelect(data) {
+      if (!data.status && !data.text && !data.type) {
+        this.url = "/api/Statistics/AlCheckList";
+        this.extraParams = {};
+      } else {
+        this.url = "/api/Statistics/AlCheckList";
+        this.extraParams = {
+          port_type: data.type * 1,
+          status: data.status ? data.status : 0,
+          content: data.text
+        };
       }
-    },
-    methods: {
-      // 搜索框选择完毕
-      onSelect(data) {
-        if (!data.status && !data.text && !data.type) {
-          this.url = "/api/Statistics/AlCheckList";
-          this.extraParams = {};
-        } else {
-          this.url = "/api/Statistics/AlCheckList";
-          this.extraParams = {
-            port_type: data.type * 1,
-            status: data.status ? data.status : 0,
-            content: data.text
-          }
-        }
-      },
     }
-
   }
+};
 </script>
 
 <style scoped>
-  .scroll-wrp {
-    height: calc(100vh - 6.5rem);
-    transition: 0.3s all;
-    height: calc(100vh - 6.5rem - constant(safe-area-inset-bottom));
-    height: calc(100vh - 6.5rem - env(safe-area-inset-bottom));
-  }
+.scroll-wrp {
+  height: calc(100vh - 6.5rem);
+  transition: 0.3s all;
+  height: calc(100vh - 6.5rem - constant(safe-area-inset-bottom));
+  height: calc(100vh - 6.5rem - env(safe-area-inset-bottom));
+}
 
-  .search-wrp {
-    height: 3.5rem;
-  }
+.search-wrp {
+  height: 3.5rem;
+}
 
+.search {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  max-width: 620px;
+  margin: 0 auto;
+}
 
-  .search {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 99;
-    max-width: 620px;
-    margin: 0 auto;
-  }
-
-  .scroll-wrp .list-wrp {
-    padding-bottom: 3rem;
-  }
+.scroll-wrp .list-wrp {
+  padding-bottom: 3rem;
+}
 </style>
